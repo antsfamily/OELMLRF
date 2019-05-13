cf4eafa0d9a6bf33825309bcd2e5e8320f156169
antsfamily <zhiliu.mind@gmail.com>
2019-05-13 08:31:13 +0800

    add OSELMLRF

---
+++ OSELMLRF.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-05-12 23:32:02
+# @Author  : Zhi Liu (zhiliu.mind@gmail.com)
+# @Link    : http://iridescent.ink
+# @Version : $1.0$
+#
+# @Note: Local Receptive Fields Based Online Extreme Learning Machine
+
+import numpy as np
+import tensorflow as tf
+
+
+class OSELMLRF(object):
+    """Class of OSELMLRF
+
+    Local Receptive Fields Based Online Extreme Learning Machine
+    """
+
+    def __init__(self, layers, name=None):
+        """Class Initialization
+
+        Initialize OSELMLRF class
+
+        Arguments:
+            layers {dict} -- ``{'input': [H, W, C], 'conv1': [kH, kW, kC]}``
+
+        Keyword Arguments:
+            name {[type]} -- [description] (default: {None})
+        """
+
+[None, nClass]
+
+
+tf.conv
